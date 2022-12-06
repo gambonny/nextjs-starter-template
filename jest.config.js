e9bd31677b627cc10b5e9dfc4a844b4c736211eb
Jonny Gamba <jonny.gamba@deliveryhero.com>
2022-12-06 09:49:19 -0500

    add jest-it-up

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,16 @@ const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: ['<rootDir>/cypress/']
+  testPathIgnorePatterns: ['<rootDir>/cypress/'],
+  coverageReporters: ['json-summary'],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0
+    }
+  }
 }
 
 module.exports = createJestConfig(customJestConfig)
